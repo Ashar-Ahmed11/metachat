79dbddc3fe56c9335accdc692db208aa4fd5fee7
Ashar <ashar.a.hmd11@gmail.com>
2023-06-17 03:05:55 +0500

    hellworld

--- db/connectmongo.js
+++ db/connectmongo.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
 const URI = 'mongodb+srv://asharahmed:ILOVEcars123@metachat.upily0h.mongodb.net/chatapp'
 
-mongoose.connect(URI, () => {     console.log("connected to mongo..") });
+// const connectToMongo = async() => {
+//   mongoose.connect(URI,await console.log('Connected To MongoDb'))
+// }
+    
+const connectToMongo=async()=>{
+    await mongoose.connect(URI)
+    console.log("Connected To Mongodb")
+}
 
 
 module.exports = connectToMongo
