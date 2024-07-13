3b9e6176423d47a6429084bd3cce54b2c256d934
Ashar <ashar.a.hmd11@gmail.com>
2024-07-13 15:30:42 -0700

    helloworld

--- db/connectmongo.js
+++ db/connectmongo.js
@@ -2,15 +2,10 @@ const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
 const URI = 'mongodb+srv://asharahmed:ILOVEcars123@metachat.upily0h.mongodb.net/chatapp'
 
-const connectToMongo = async () => {
-  try {
-    await mongoose.connect(URI)
-    console.log("Connect to MongoDB successfully")
-  } catch (error) {
-    console.log("Connect failed " + error.message)
-  }
+const connectToMongo = async() => {
+  mongoose.connect(URI,await console.log('Connected To MongoDb'))
 }
-
+    
 
 
 module.exports = connectToMongo
