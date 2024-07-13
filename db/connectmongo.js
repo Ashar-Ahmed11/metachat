6dfb351fd958b00792df400c50ff1326d8e45f40
Ashar <ashar.a.hmd11@gmail.com>
2024-07-13 14:45:47 -0700

    dsdsadsa

--- db/connectmongo.js
+++ db/connectmongo.js
@@ -3,7 +3,20 @@ mongoose.set("strictQuery", false);
 const URI = 'mongodb+srv://asharahmed:ILOVEcars123@metachat.upily0h.mongodb.net/chatapp'
 
 const connectToMongo = async() => {
-  await mongoose.connect(URI,await console.log('Connected To MongoDb'))
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 50000, // Increase the timeout value
+      socketTimeoutMS: 45000, // Increase the timeout value
+   
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    // Retry connection after a delay if needed
+    setTimeout(connectToMongo, 5000);
+  }
 }
     
 
